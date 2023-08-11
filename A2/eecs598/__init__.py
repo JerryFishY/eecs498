a024b62c12cc0e2c72ddb406fd19b7dd5c294ea5
JerryFishY <1446321377y@gmail.com>
2023-08-11 22:09:45 +0800

    add the 2022-ver assignment2

--- A2/eecs598/__init__.py
+++ A2/eecs598/__init__.py
@@ -1,5 +1,2 @@
-from .vis import tensor_to_image, visualize_dataset
-from . import data
-from . import grad
-from . import submit
-from .utils import reset_seed
+from . import data, grad, submit
+from .utils import reset_seed, tensor_to_image, visualize_dataset
